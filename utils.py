7c103bc983b79f688cfc53db9ab33f4b21bccdbc
lovecom9001 <55788332+lovecom9001@users.noreply.github.com>
2024-09-09 00:02:32 +0900

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,7 @@
+def print_messages():
+# 이전 대화기록을 출력해 주는 코드
+    if "messages" in st.session_state and len(st.session_state["messages"]) > 0:
+        for role, message in st.session_state["messages"]:
+            st.chat_message(role).write(message)
+    
+print_messages()
